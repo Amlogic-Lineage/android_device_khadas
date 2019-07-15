e7cb02ac8d205633991eae1c78ce76545944a09f
terry <terry@szwesion.com>
2019-07-15 09:54:59 +0800

    Compile: fix dvb_device unknown issue

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,7 +1,3 @@
 BOARD_SEPOLICY_DIRS += \
     device/khadas/common/sepolicy \
     device/khadas/common/sepolicy/aml_core
-ifneq ($(TARGET_BUILD_GOOGLE_ATV), true)
-BOARD_SEPOLICY_DIRS += \
-    device/google/atv/sepolicy
-endif
