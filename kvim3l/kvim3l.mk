1c1c692a2e2aceda9d110723a7800d6115ed7f8a
Terry <terry@khadas.com>
2019-08-21 10:01:02 +0800

    Language: turn off CEC auto change language [VIM3L]

--- kvim3l/kvim3l.mk
+++ kvim3l/kvim3l.mk
@@ -79,6 +79,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
         ro.build.display.id=VIM3L_Pie_$(shell date +%Y%m%d)
 
+PRODUCT_PROPERTY_OVERRIDES += \
+        persist.vendor.sys.cec.set_menu_language=false
+
 PRODUCT_NAME := kvim3l
 PRODUCT_DEVICE := kvim3l
 PRODUCT_BRAND := Khadas
