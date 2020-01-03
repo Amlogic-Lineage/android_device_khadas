cffa4bec9c95cfd8277391355b84cc3f84b82d59
Terry <terry@khadas.com>
2020-01-03 17:57:14 +0800

    OTA: add OTA update url

--- kvim/kvim.mk
+++ kvim/kvim.mk
@@ -439,6 +439,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 #########################################################################
 BUILD_WITH_GAPPS_CONFIG := false
 
+
+#Khadas OTA
+PRODUCT_PROPERTY_OVERRIDES += \
+   ro.product.firmware=00900001 \
+   ro.product.otaupdateurl=http://dl.khadas.com:8089/otaupdate/update
+
 #########################################################################
 #
 #                                     A/B update
