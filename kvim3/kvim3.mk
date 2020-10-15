08f023a6bc80c2ffccf57c468aaffd38a17abf72
Terry <terry@khadas.com>
2020-10-15 16:43:40 +0800

    OTA: update VIM3/VIM3L ota firmware version

--- kvim3/kvim3.mk
+++ kvim3/kvim3.mk
@@ -437,7 +437,7 @@ BUILD_WITH_GAPPS_CONFIG := false
 
 #Khadas OTA
 PRODUCT_PROPERTY_OVERRIDES += \
-   ro.product.firmware=00903005 \
+   ro.product.firmware=00903006 \
    ro.product.otaupdateurl=http://dl.khadas.com:8089/otaupdate/update
 
 #########################################################################
